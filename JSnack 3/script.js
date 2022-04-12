6bd3aac8835ca1b193782006060d0d7662bab71d
Amanjit313 <amanjitsingh313@gmail.com>
2022-04-12 16:44:31 +0200

    JSnack 3 - Complete

---
+++ JSnack 3/script.js
@@ -0,0 +1,10 @@
+let pNumber = parseInt (prompt("Inserisci il primo numero"));
+let sNumber = parseInt (prompt("Inserisci il secondo numero"));
+let tNumber = parseInt (prompt("Inserisci il terzo numero"));
+let qNumber = parseInt (prompt("Inserisci il quarto numero"));
+let quNumber =parseInt ( prompt("Inserisci il quinto numero"));
+
+let sommanumeri = pNumber + sNumber + tNumber + qNumber + quNumber;
+console.log(sommanumeri);
+document.getElementById("somma-numeri").innerHTML = sommanumeri;
+
