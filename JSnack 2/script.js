cd5ba05222b6cbb96e081c043d00279c95a964eb
Amanjit313 <amanjitsingh313@gmail.com>
2022-04-12 16:36:29 +0200

    JSnack 2 - Complete

---
+++ JSnack 2/script.js
@@ -0,0 +1,18 @@
+/* 
+  L’utente inserisce due parole in successione, con due prompt.
+  Il software stampa prima la parola più corta, poi la parola più lunga.
+
+ PASSAGGI
+  - Usare due prompt con possilità di inserire parole
+  - Stampare prima la parola più corta poi la più lunga
+*/
+
+let primaparola = prompt("Inserisci la prima parola");
+
+let secondaparola = prompt("Inserisci la seconda parola");
+
+if (primaparola.length > secondaparola.length){
+  document.getElementById("parola").innerHTML = primaparola + secondaparola;
+} else {
+  document.getElementById("parola").innerHTML = secondaparola + primaparola;
+}
